fa33ea6217fc8c9634ad49313cc14da5539597ab
maiasullivan <s3942603@student.rmit.edu.au>
2024-07-14 21:40:22 +1000

    navigation routes

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, {Suspense} from 'react'
+import {Routes, Route} from "react-router-dom"
+import Layout from '../layouts/Layout';
+import AdminLayout from '../layouts/AdminLayout';
+import { AdminHome, Home, UserProfile } from '../pages';
 
 const App = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center text-blue-600 font-semibold">App</div>
+    <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+            <Route element={<Layout />}>
+                <Route path="/" element={<Home />}/>
+                <Route path="/profile/:uid" element={<UserProfile />}/>
+            </Route>
+            
+
+                <Route path="/admin/*" element={<AdminLayout />}>
+                    <Route index element={<AdminHome />} />
+            </Route>
+        </Routes>
+    </Suspense>
   );
 };
 
---
+++ client/src/pages/index.js
@@ -0,0 +1,5 @@
+export {default as Home} from "./Home";
+export {default as UserProfile} from "./UserProfile";
+
+
+export {default as AdminHome} from "./admin/AdminHome";

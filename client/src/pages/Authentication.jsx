a096e35c3906c16fa4d3c40d154ae4894871f2d3
maiasullivan <s3942603@student.rmit.edu.au>
2024-07-15 22:47:09 +1000

    gmail access

--- client/src/pages/Authentication.jsx
+++ client/src/pages/Authentication.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { LoginBG } from "../assets";
+import { FcGoogle } from "react-icons/fc"
 
 const Authentication = () => {
   return (
@@ -12,9 +13,16 @@ const Authentication = () => {
         }}
         className="w-screen h-screen flex justify-center px-4 py-6"
         >
-            <div className="w-full lg:w-96 px-4 py-6 rounded-md backdrop-blur-md flex items-center justify-center flex-col">
+            <div className="w-full lg:w-96 px-4 py-6 rounded-md backdrop-blur-md flex 
+            items-center justify-center flex-col">
                 <p className="text-2xl">Welcome Back!</p>
                 <p>Sign in to access your store</p>
+
+                <div className="w-full px-4 py-3 flex items-center justify-center 
+                border border-gray-400 cursor-pointer rounded-md">
+                    <FcGoogle className="text-2xl" />
+                    <p>Sign in with Gmail</p>
+                </div>
             </div>
     </div>
   );

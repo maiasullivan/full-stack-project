6964791d77b25790db09c3369d26df6ce1303fde
maiasullivan <s3942603@student.rmit.edu.au>
2024-07-15 22:31:33 +1000

    Welcome Back!

--- client/src/pages/Authentication.jsx
+++ client/src/pages/Authentication.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import { LoginBG } from "../assets"
+import React from 'react';
+import { LoginBG } from "../assets";
 
 const Authentication = () => {
   return (
-    <div>
+    <div
         style={{
             background: `url(${LoginBG})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat'
         }}
+        className="w-screen h-screen flex justify-center px-4 py-6"
+        >
+            <div className="w-full lg:w-96 px-4 py-6 rounded-md backdrop-blur-md">
+                <p>Welcome Back!</p>
+            </div>
     </div>
-  )
-}
+  );
+};
 
-export default Authentication
+export default Authentication;
